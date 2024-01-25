a4b647a6396f1edfaf0272afbc016134b0bf9023
RiShU00007 <119649513+RiShU00007@users.noreply.github.com>
2024-01-25 10:28:52 +0530

    Add files via upload

---
+++ a.js
@@ -0,0 +1,126 @@
+// function exampleFunction() {
+//     if(true){
+//         var functionScopedVar="I am function-shaped";
+//         let blockScopedLet="I am block-scoped";
+//         const blockScopedConst="I am constant and block-scoped";
+    
+//     console.log(functionScopedVar);
+//     console.log(blockScopedLet);
+//     console.log(blockScopedConst);
+//     }
+
+// }
+// exampleFunction();
+// console.log(x);
+// let x=3;
+// console.log(x);
+// hello();
+// function hello() {
+//     console.log("Hello, world!");
+
+// }
+// let exampleVar="Hello";
+// console.log(typeof(exampleVar));
+// let result= 5 + "5";
+// console.log(result);   //string
+
+// let age=25;
+// console.log(typeof(age)); //number
+
+// let isStudent=true;
+// console.log(typeof(isStudent)); //boolean
+
+// let undefined;
+// console.log(typeof(undefined)); //undefined
+
+// let nullVar=null;
+// console.log(typeof(nullVar)); //object-A collection of Key value pairs.
+
+// let person={
+//     name:"Alice",
+//     age: 30,
+//     isStudent: false
+// };
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.isStudent);
+
+// //Arrays
+// let fruits=[1,"Banana","orange"];
+// console.log(typeof(fruits));
+// console.log(fruits[0]);
+// console.log(fruits[2]);
+
+// //Functions
+
+// function add(a,b) {
+//     return a+b;
+// }
+// var a=add(2,3);
+// console.log(a);
+
+// let Result = 5 + "5";
+// console.log(Result);
+
+// let num = 5 - "5";
+// console.log(num);
+
+// let Num=5 * "5";
+// console.log(Num);
+
+// //Arithematic ops
+
+// let y=3;
+// let z=2;
+
+// console.log(y + z);
+// console.log(y - z);
+// console.log(y * z);
+// console.log(y/z);
+// console.log(y % z);
+
+// //Comparison Ops
+// let l=10
+// let m=5;
+// console.log(l>m);      
+// console.log(l<m);
+// console.log(l>=m);
+// console.log(l<=m);
+// console.log(l === m);
+// console.log(l !== m);
+//Logical ops
+
+let p=true;
+let q=false;
+
+console.log(p && q);
+console.log(p || q);
+console.log(!p);
+
+//Assignment ops
+let num = 10;
+num += 5;
+num -= 3;
+num *= 2;
+num /= 4;
+
+//For loop
+for (let i=0; i<5 ; i++){
+    console.log(i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
